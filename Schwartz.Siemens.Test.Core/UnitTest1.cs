f46d21aa6ff858a8cab78a766c6dcad29758950a
Axl <makosful@gmail.com>
2018-11-22 09:55:03 +0100

    Added Travis CI

---
+++ Schwartz.Siemens.Test.Core/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Schwartz.Siemens.Test.Core
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
