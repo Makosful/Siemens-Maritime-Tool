66ecfd1a9337b4179f07cc3c62c748c7405f452f
Axl <makosful@gmail.com>
2018-11-26 09:51:07 +0100

    Created CRUD interfaces

---
+++ Schwartz.Siemens.Core/DomainServices/ICrudRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Schwartz.Siemens.Core.DomainServices
+{
+    public interface ICrudRepositoryRead<T>
+    {
+        T Read(int id);
+
+        IEnumerable<T> ReadAll();
+    }
+
+    public interface ICrudRepositoryWrite<T>
+    {
+        T Create(T item);
+
+        T Update(int id, T item);
+
+        T Delete(int id);
+    }
+
+    public interface ICrudRepository<T> : ICrudRepositoryRead<T>, ICrudRepositoryWrite<T>
+    {
+    }
+}
