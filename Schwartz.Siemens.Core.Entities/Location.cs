1864b84ed7e048c7f1161deb16c9dac232795cac
Axl <makosful@gmail.com>
2018-11-26 12:04:51 +0100

    Added two base entities

---
+++ Schwartz.Siemens.Core.Entities/Location.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Schwartz.Siemens.Core.Entities
+{
+    public class Location
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public DateTime Date { get; set; }
+        public Rig Rig { get; set; }
+    }
+}
---
+++ Schwartz.Siemens.Core.Entities/Rig.cs
@@ -0,0 +1,9 @@
+﻿namespace Schwartz.Siemens.Core.Entities
+{
+    public class Rig
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Location Location { get; set; }
+    }
+}
