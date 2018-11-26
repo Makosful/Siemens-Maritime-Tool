674a4b538db0929f0d300d9e180b2ed36fb9a197
Axl <makosful@gmail.com>
2018-11-26 12:08:11 +0100

    Update Rig.cs

--- Schwartz.Siemens.Core.Entities/Rig.cs
+++ Schwartz.Siemens.Core.Entities/Rig.cs
@@ -1,9 +1,11 @@
-﻿namespace Schwartz.Siemens.Core.Entities
+﻿using System.Collections.Generic;
+
+namespace Schwartz.Siemens.Core.Entities
 {
     public class Rig
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Location Location { get; set; }
+        public List<Location> Location { get; set; }
     }
 }
