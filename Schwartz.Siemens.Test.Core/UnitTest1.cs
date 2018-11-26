a9c881fd81b1caa31f4be1fef2c334e2871e1fcc
Axl <makosful@gmail.com>
2018-11-26 12:19:47 +0100

    Beautified empty test classes

--- Schwartz.Siemens.Test.Core/UnitTest1.cs
+++ Schwartz.Siemens.Test.Core/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace Schwartz.Siemens.Test.Core
@@ -8,7 +7,6 @@ namespace Schwartz.Siemens.Test.Core
         [Fact]
         public void Test1()
         {
-
         }
     }
 }
