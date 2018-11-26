a4d2e6e8e4fa9c3be18c87ba18daec02b1c9ecae
Axl <makosful@gmail.com>
2018-11-26 11:42:28 +0100

    Create EstablishHostedServices.cs

---
+++ Schwartz.Siemens.Core/HostedServices/EstablishHostedServices.cs
@@ -0,0 +1,13 @@
+﻿using Hangfire;
+using System;
+
+namespace Schwartz.Siemens.Core.HostedServices
+{
+    public class EstablishHostedServices
+    {
+        public EstablishHostedServices()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("My awesome message"), Cron.Minutely);
+        }
+    }
+}
