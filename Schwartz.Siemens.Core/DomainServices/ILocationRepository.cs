1bc8379be000e16765dd395354b3d7e3e22bf10c
Axl <makosful@gmail.com>
2018-11-27 14:27:42 +0100

    Added ILocationRepository and its implementation

---
+++ Schwartz.Siemens.Core/DomainServices/ILocationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Schwartz.Siemens.Core.Entities.Rigs;
+using System.Collections.Generic;
+
+namespace Schwartz.Siemens.Core.DomainServices
+{
+    public interface ILocationRepository : ICrudRepository<Location>
+    {
+        void CreateRange(IEnumerable<Location> locations);
+    }
+}
---
+++ Schwartz.Siemens.Infrastructure.Data/Repositories/LocationRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Schwartz.Siemens.Core.DomainServices;
+using Schwartz.Siemens.Core.Entities.Rigs;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Schwartz.Siemens.Infrastructure.Data.Repositories
+{
+    public class LocationRepository : ILocationRepository
+    {
+        public LocationRepository(MaritimeContext context)
+        {
+            Context = context;
+        }
+
+        private MaritimeContext Context { get; }
+
+        public Location Read(int id)
+        {
+            return Context.Locations
+                .Include(loc => loc.Rig)
+                .FirstOrDefault(loc => loc.Id == id);
+        }
+
+        public IEnumerable<Location> ReadAll()
+        {
+            return Context.Locations
+                .Include(loc => loc.Rig);
+        }
+
+        public Location Create(Location item)
+        {
+            return Context.Locations.Add(item).Entity;
+        }
+
+        public Location Update(int id, Location item)
+        {
+            item.Id = id;
+            var location = Context.Locations.Update(item).Entity;
+            Context.SaveChanges();
+            return location;
+        }
+
+        public Location Delete(int id)
+        {
+            var location = Read(id);
+            Context.Locations.Remove(location);
+            Context.SaveChanges();
+            return location;
+        }
+
+        public void CreateRange(IEnumerable<Location> locations)
+        {
+            Context.Locations.AddRange(locations);
+            Context.SaveChanges();
+        }
+    }
+}
