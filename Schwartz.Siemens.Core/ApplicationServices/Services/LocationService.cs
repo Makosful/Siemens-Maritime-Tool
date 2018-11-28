ee3929b1eb0a538d7cdebe27096e3319aed1b0ce
Axl <makosful@gmail.com>
2018-11-28 15:59:10 +0100

    Added the remaining CRUD for the Location entities

---
+++ Schwartz.Siemens.Core/ApplicationServices/Services/LocationService.cs
@@ -0,0 +1,42 @@
+﻿using Schwartz.Siemens.Core.DomainServices.Repositories;
+using Schwartz.Siemens.Core.Entities.Rigs;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Schwartz.Siemens.Core.ApplicationServices.Services
+{
+    public class LocationService : ILocationService
+    {
+        public LocationService(ILocationRepository repository)
+        {
+            LocationRepository = repository;
+        }
+
+        private ILocationRepository LocationRepository { get; }
+
+        public Location Read(int id)
+        {
+            return LocationRepository.Read(id);
+        }
+
+        public List<Location> ReadAll()
+        {
+            return LocationRepository.ReadAll().ToList();
+        }
+
+        public Location Create(Location item)
+        {
+            return LocationRepository.Create(item);
+        }
+
+        public Location Update(int id, Location item)
+        {
+            return LocationRepository.Update(id, item);
+        }
+
+        public Location Delete(int id)
+        {
+            return LocationRepository.Delete(id);
+        }
+    }
+}
---
+++ Schwartz.Siemens.Ui.RestApi/Controllers/LocationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Schwartz.Siemens.Core.ApplicationServices;
+using Schwartz.Siemens.Core.Entities.Rigs;
+using System.Collections.Generic;
+
+namespace Schwartz.Siemens.Ui.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        public LocationController(ILocationService locationService)
+        {
+            LocationService = locationService;
+        }
+
+        private ILocationService LocationService { get; }
+
+        [HttpGet]
+        public ActionResult<List<Location>> GetAllLocations()
+        {
+            return Ok(LocationService.ReadAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Location> GetLocationById(int id)
+        {
+            return Ok(LocationService.Read(id));
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public ActionResult<Location> CreateLocation([FromBody] Location location)
+        {
+            return Ok(LocationService.Create(location));
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public ActionResult<Location> UpdateLocation(int id, [FromBody] Location location)
+        {
+            return Ok(LocationService.Update(id, location));
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public ActionResult<Location> DeleteLocation(int id)
+        {
+            return Ok(LocationService.Delete(id));
+        }
+    }
+}
