0df36c96dd87aa4f459560a6f543286faa24a2f5
Axl <makosful@gmail.com>
2018-11-29 11:16:54 +0100

    Renamed EstablishedHostedService to HostedServices

---
+++ Schwartz.Siemens.Core/HostedServices/HostedServices.cs
@@ -0,0 +1,28 @@
+﻿using Hangfire;
+using Schwartz.Siemens.Core.DomainServices.Repositories;
+using System.Linq;
+
+namespace Schwartz.Siemens.Core.HostedServices
+{
+    public class HostedServices : IHostedService
+    {
+        public HostedServices(IRigRepository rigRepository)
+        {
+            RigRepository = rigRepository;
+        }
+
+        private IRigRepository RigRepository { get; }
+
+        public void StartHostedServices()
+        {
+            // RecurringJob.AddOrUpdate(() => TimedRigUpdates(), Cron.HourInterval(12));
+            RecurringJob.AddOrUpdate("location-update", () => TimedRigUpdates(), Cron.Minutely);
+        }
+
+        private void TimedRigUpdates()
+        {
+            var rigs = RigRepository.ReadAll();
+            RigRepository.UpdatePositions(rigs.Select(r => r.Id));
+        }
+    }
+}
