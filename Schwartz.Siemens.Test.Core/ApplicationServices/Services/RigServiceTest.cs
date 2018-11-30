2751028674c5d4ed11fbff5c251f8791050db192
Axl <makosful@gmail.com>
2018-11-30 12:42:37 +0100

    Renamed and moved unit test into it's correct folder/namespace

---
+++ Schwartz.Siemens.Test.Core/ApplicationServices/Services/RigServiceTest.cs
@@ -0,0 +1,16 @@
+using Moq;
+using Schwartz.Siemens.Core.ApplicationServices;
+using Schwartz.Siemens.Core.ApplicationServices.Services;
+using Schwartz.Siemens.Core.DomainServices.Repositories;
+using Schwartz.Siemens.Core.Entities.Rigs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Schwartz.Siemens.Test.Core.ApplicationServices.Services
+{
+    public class RigServiceTest
+    {
+    }
+}
