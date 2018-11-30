198025ce216acb25004409fff63c4a51ac9103a1
Axl <makosful@gmail.com>
2018-11-30 12:44:59 +0100

    Wrote a unit test for RigService.Create

--- Schwartz.Siemens.Test.Core/ApplicationServices/Services/RigServiceTest.cs
+++ Schwartz.Siemens.Test.Core/ApplicationServices/Services/RigServiceTest.cs
@@ -12,5 +12,107 @@ namespace Schwartz.Siemens.Test.Core.ApplicationServices.Services
 {
     public class RigServiceTest
     {
+        private static Mock<IRigRepository> CreateMoqRepository()
+        {
+            var repository = new Mock<IRigRepository>();
+
+            // Setup create
+            repository.Setup(rigRepository => rigRepository.Create(It.IsAny<Rig>()))
+                .Returns((Rig rig) => rig);
+
+            // Setup ReadAll
+            repository.Setup(rigRepository => rigRepository.ReadAll())
+                .Returns(MockRigs);
+
+            // Setup Read single
+            for (var i = 0; i < 3; i++)
+            {
+                var i1 = i;
+                repository.Setup(rigRepository =>
+                        rigRepository.Read(i1 + 1))
+                    .Returns(MockRigs().ToList()[i]);
+            }
+
+            return repository;
+        }
+
+        private static IEnumerable<Rig> MockRigs()
+        {
+            var rigs = new List<Rig>
+            {
+                new Rig {Id = 1, Name = "Number One"},
+                new Rig {Id = 2, Name = "Number Two"},
+                new Rig {Id = 3, Name = "Number Three"}
+            };
+
+            return rigs;
+        }
+
+        [Fact]
+        public void RigService_Create_UnsatId_ExpectsException()
+        {
+            var repository = CreateMoqRepository();
+            IRigService service = new RigService(repository.Object);
+
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                service.Create(new Rig()));
+
+            Assert.NotNull(exception);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void RigService_Create_ExistingRigId_ExpectsException(int id)
+        {
+            var repository = CreateMoqRepository();
+            IRigService service = new RigService(repository.Object);
+
+            var exception = Assert.Throws<ArgumentException>(() =>
+                service.Create(new Rig() { Id = id }));
+
+            Assert.NotNull(exception);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-5)]
+        [InlineData(-10)]
+        public void RigService_Create_NegativeId_ExpectsException(int id)
+        {
+            var repository = CreateMoqRepository();
+            IRigService service = new RigService(repository.Object);
+
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() =>
+                service.Create(new Rig { Id = id }));
+
+            Assert.NotNull(exception);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(15)]
+        public void RigService_Create_ValidRigParam_ExpectsSameRigReturned(int id)
+        {
+            var repository = CreateMoqRepository();
+            IRigService service = new RigService(repository.Object);
+
+            var expectedRig = new Rig()
+            {
+                Id = id,
+                Location = new List<Location>(),
+                Name = $"Number {id}"
+            };
+
+            var actualRig = service.Create(expectedRig);
+
+            repository.Verify(
+                r => r.Create(It.IsAny<Rig>()),
+                Times.AtLeastOnce);
+            Assert.NotNull(actualRig);
+            Assert.Equal(expectedRig.Id, actualRig.Id);
+        }
     }
 }
