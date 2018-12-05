c808a3da0f97164aea46b7b8980eeb75d5dda92a
Axl <makosful@gmail.com>
2018-12-05 17:47:51 +0100

    Added Username to User entities

--- Schwartz.Siemens.Core.Entities/UserBase/User.cs
+++ Schwartz.Siemens.Core.Entities/UserBase/User.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public string Email { get; set; }
+        public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public bool IsAdmin { get; set; }
