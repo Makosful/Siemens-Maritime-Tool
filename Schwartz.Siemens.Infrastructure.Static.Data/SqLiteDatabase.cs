544fc5198cf1460949bc32ef17dd0e9f908b3375
Axl <makosful@gmail.com>
2018-12-05 16:12:48 +0100

    Added a SqLiteDatabase class to initialize production databases outside of Azure

---
+++ Schwartz.Siemens.Infrastructure.Static.Data/SqLiteDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Schwartz.Siemens.Core.DomainServices;
+using Schwartz.Siemens.Infrastructure.Data;
+
+namespace Schwartz.Siemens.Infrastructure.Static.Data
+{
+    public class SqLiteDatabase : IDatabase
+    {
+        public SqLiteDatabase(MaritimeContext context)
+        {
+            Context = context;
+        }
+
+        private MaritimeContext Context { get; }
+
+        public void Initialize()
+        {
+            Context.Database.EnsureCreated();
+        }
+    }
+}
