cd6c4acc280758cb495ee5f9965e2c59454d9617
Axl <makosful@gmail.com>
2018-12-05 17:49:12 +0100

    Formatted SqlDatabase

--- Schwartz.Siemens.Infrastructure.Static.Data/SqlDatabase.cs
+++ Schwartz.Siemens.Infrastructure.Static.Data/SqlDatabase.cs
@@ -1,20 +1,24 @@
-﻿using Schwartz.Siemens.Core.DomainServices;
+﻿using Schwartz.Siemens.Core.ApplicationServices;
+using Schwartz.Siemens.Core.DomainServices;
 using Schwartz.Siemens.Infrastructure.Data;
 
 namespace Schwartz.Siemens.Infrastructure.Static.Data
 {
     public class SqlDatabase : IDatabase
     {
-        public SqlDatabase(MaritimeContext context)
+        public SqlDatabase(MaritimeContext context, IAuthenticationHelper authenticationHelper)
         {
             Context = context;
+            AuthenticationHelper = authenticationHelper;
         }
+
+        private IAuthenticationHelper AuthenticationHelper { get; }
+
+        private MaritimeContext Context { get; }
 
         public void Initialize()
         {
             Context.Database.EnsureCreated();
         }
-
-        private MaritimeContext Context { get; }
     }
 }
