a3a520c7bbee4f864098a11e58fc7b955e429d91
Axl <makosful@gmail.com>
2018-12-06 14:22:51 +0100

    Removed UpdateLocationAsunc method from IRigService

--- Schwartz.Siemens.Core/ApplicationServices/IRigService.cs
+++ Schwartz.Siemens.Core/ApplicationServices/IRigService.cs
@@ -4,8 +4,6 @@ namespace Schwartz.Siemens.Core.ApplicationServices
 {
     public interface IRigService : ICrudService<Rig>
     {
-        Location UpdateLocation(int imo);
-
-        void UpdateLocationAsync(int imo);
+        Rig UpdateLocation(int imo);
     }
 }
