90df608379fda1bd69e1e31d3cbf199f109fcf52
Axl <makosful@gmail.com>
2018-12-06 15:44:17 +0100

    Removed ID from Rig entity again

--- Schwartz.Siemens.Core.Entities/Rigs/Rig.cs
+++ Schwartz.Siemens.Core.Entities/Rigs/Rig.cs
@@ -4,11 +4,6 @@ namespace Schwartz.Siemens.Core.Entities.Rigs
 {
     public class Rig
     {
-        /// <summary>
-        /// ID Is for database only. Try ot rely on IMO instead
-        /// </summary>
-        public int Id { get; set; }
-
         public int Imo { get; set; }
         public string Name { get; set; }
         public List<Location> Locations { get; set; }
