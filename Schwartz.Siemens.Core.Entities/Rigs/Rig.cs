435d3cdc439ac2b04a26db1ff8a90e2879aaf831
Axl <makosful@gmail.com>
2018-12-18 12:42:06 +0100

    Updated Rig entity to include rig type

--- Schwartz.Siemens.Core.Entities/Rigs/Rig.cs
+++ Schwartz.Siemens.Core.Entities/Rigs/Rig.cs
@@ -7,8 +7,7 @@ namespace Schwartz.Siemens.Core.Entities.Rigs
         public int Imo { get; set; }
         public string Name { get; set; }
         public List<Location> Locations { get; set; } = new List<Location>();
-        public string Type { get; set; }
-        public string Label { get; set; }
+        public string RigType { get; set; }
         public bool Outdated { get; set; }
     }
 }
